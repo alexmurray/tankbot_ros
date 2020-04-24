7ad512551e9c2056af063cef45a5b681185cf00a
Alex Murray <murray.alex@gmail.com>
2020-04-24 14:01:43 +0000

    Port to teleop
    Should we use the more regular name of joy/cmd_vel?

--- src/tankbot/src/Robot.py
+++ src/tankbot/src/Robot.py
@@ -3,13 +3,9 @@
 # example of using this class.
 # Author: Tony DiCola
 # License: MIT License https://opensource.org/licenses/MIT
-import time
 import atexit
 
 from Raspi_MotorHAT import Raspi_MotorHAT, Raspi_DCMotor
-
-
-# from Adafruit_MotorHAT import Adafruit_MotorHAT
 
 
 class Robot(object):
@@ -18,7 +14,7 @@ class Robot(object):
         """Create an instance of the robot.  Can specify the following optional
         parameters:
          - addr: The I2C address of the motor HAT, default is 0x6f.
-         - left_id: The ID of the left motor, default is 1.
+         - left_id: The ID of the left motor, default is 3.
          - right_id: The ID of the right motor, default is 2.
          - left_trim: Amount to offset the speed of the left motor, can be positive
                       or negative and use useful for matching the speed of both
@@ -32,8 +28,8 @@ class Robot(object):
         self._mh = Raspi_MotorHAT(addr)
         self._left = self._mh.getMotor(left_id)
         self._right = self._mh.getMotor(right_id)
-        self._left_trim = left_trim
-        self._right_trim = right_trim
+        self._left_trim = int(left_trim)
+        self._right_trim = int(right_trim)
         # Start with motors turned off.
         self._left.run(Raspi_MotorHAT.RELEASE)
         self._right.run(Raspi_MotorHAT.RELEASE)
@@ -44,7 +40,7 @@ class Robot(object):
     def _left_speed(self, speed):
         """Set the speed of the left motor, taking into account its trim offset.
         """
-        assert 0 <= speed <= 255, 'Speed must be a value between 0 to 255 inclusive!'
+        assert 0 <= speed <= 255, 'Speed %d must be a value between 0 to 255 inclusive!' % speed
         speed += self._left_trim
         speed = max(0, min(255, speed))  # Constrain speed to 0-255 after trimming.
         self._left.setSpeed(speed)
@@ -52,7 +48,7 @@ class Robot(object):
     def _right_speed(self, speed):
         """Set the speed of the right motor, taking into account its trim offset.
         """
-        assert 0 <= speed <= 255, 'Speed must be a value between 0 to 255 inclusive!'
+        assert 0 <= speed <= 255, 'Speed %d must be a value between 0 to 255 inclusive!' % speed
         speed += self._right_trim
         speed = max(0, min(255, speed))  # Constrain speed to 0-255 after trimming.
         self._right.setSpeed(speed)
@@ -62,63 +58,16 @@ class Robot(object):
         self._left.run(Raspi_MotorHAT.RELEASE)
         self._right.run(Raspi_MotorHAT.RELEASE)
 
-    def forward(self, speed, seconds=None):
-        """Move forward at the specified speed (0-255).  Will start moving
-        forward and return unless a seconds value is specified, in which
-        case the robot will move forward for that amount of time and then stop.
-        """
+    def right(self, speed_percent):
+        """Drive right motor at speed_percent"""
         # Set motor speed and move both forward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Raspi_MotorHAT.FORWARD)
-        self._right.run(Raspi_MotorHAT.FORWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
+        dir = Raspi_MotorHAT.FORWARD if speed_percent >= 0 else Raspi_MotorHAT.BACKWARD
+        self._right.run(dir)
+        self._right_speed(int(abs(speed_percent) * 255 / 100))
 
-    def backward(self, speed, seconds=None):
-        """Move backward at the specified speed (0-255).  Will start moving
-        backward and return unless a seconds value is specified, in which
-        case the robot will move backward for that amount of time and then stop.
-        """
-        # Set motor speed and move both backward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Raspi_MotorHAT.BACKWARD)
-        self._right.run(Raspi_MotorHAT.BACKWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
-
-    def right(self, speed, seconds=None):
-        """Spin to the right at the specified speed.  Will start spinning and
-        return unless a seconds value is specified, in which case the robot will
-        spin for that amount of time and then stop.
-        """
+    def left(self, speed_percent):
+        """Drive left motor at speed_percent"""
         # Set motor speed and move both forward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Raspi_MotorHAT.FORWARD)
-        self._right.run(Raspi_MotorHAT.BACKWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
-
-    def left(self, speed, seconds=None):
-        """Spin to the left at the specified speed.  Will start spinning and
-        return unless a seconds value is specified, in which case the robot will
-        spin for that amount of time and then stop.
-        """
-        # Set motor speed and move both forward.
-        self._left_speed(speed)
-        self._right_speed(speed)
-        self._left.run(Raspi_MotorHAT.BACKWARD)
-        self._right.run(Raspi_MotorHAT.FORWARD)
-        # If an amount of time is specified, move for that time and then stop.
-        if seconds is not None:
-            time.sleep(seconds)
-            self.stop()
-    
+        dir = Raspi_MotorHAT.FORWARD if speed_percent >= 0 else Raspi_MotorHAT.BACKWARD
+        self._left.run(dir)
+        self._left_speed(int(abs(speed_percent) * 255 / 100))
